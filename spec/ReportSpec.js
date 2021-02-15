90c8406b0912775deafd5c91e25ba4093a0a24e0
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-02-15 14:02:03 +0000

    added function for non red, amber, green grades input. Tests pass

--- spec/ReportSpec.js
+++ spec/ReportSpec.js
@@ -25,11 +25,19 @@ describe('Report', () => {
       expect(test.gradeSummary("Green, Amber, Amber, Red, Red")).toEqual("Green: 1\nAmber: 2\nRed: 2")
     })
 
-    it('takes a different input and outputs count of grades given', () => {
-      spyOn(test, 'gradeCount').and.returnValue([8,9,10])
+    it('mocked gradeCount to test output format 1', () => {
+      spyOn(test, 'gradeCount').and.returnValue([8,9,10,0])
       expect(test.gradeSummary("Green, Amber, Amber, Red, Red")).toEqual("Green: 8\nAmber: 9\nRed: 10")
     })
   })
+
+  describe('Edge case', ()=> {
+    it('mocked gradeCount to test output format 2', () => {
+      expect(test.gradeSummary("Green, Dave, Whimsy, Red")).toEqual("Green: 1\nRed: 1\nUncounted: 2")
+    })
+  })
+
+
 
 
 })
